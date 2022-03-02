c4d78fb2004e2c02945dbe1b1768e849128cbbab
Константин Евлампиев <kevlamiev@gmail.com>
2022-03-03 00:35:13 +0700

    first task. Lite version

---
+++ lesson2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data;
+
+namespace lesson2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                //Нет уверенности, что введут данные правильно
+                Console.WriteLine("Enter minimum temperature (using ',' as separator ): ");
+                float minTmpTemp = float.Parse(Console.ReadLine());
+                
+                Console.WriteLine("Enter maximum temperature (using ',' as separator ): ");
+                float maxTmpTemp = float.Parse(Console.ReadLine());
+                
+                //Если что, температуры меняем местами
+                float minT = Math.Min(minTmpTemp, maxTmpTemp);
+                float maxT = Math.Max(minTmpTemp, maxTmpTemp);
+                
+                Console.WriteLine($"Minimal temperature {minT}, " +
+                                  $"maximum temperature {maxT}, " +
+                                  $"average temperature {(minT+maxT)/2}");
+            }
+            catch (SyntaxErrorException errorException)
+            {
+                Console.WriteLine(errorException.Message);
+            }
+        }
+    }
+}
