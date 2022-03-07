7f98373d9e12d34c989abfc122f4134954079f15
kevlampiev <kevlampiev@gmail.com>
2022-03-07 13:28:32 +0700

    some decoration

--- lesson2b/Program.cs
+++ lesson2b/Program.cs
@@ -10,22 +10,28 @@ namespace lesson2b
     {
         static void Main(string[] args)
         {
-            string shopName = "ООО 'Электротовары'";
+            string shopName = "ООО 'ДНС Ритейл'";
+            string shopAddress = "115372, г.Москва, ул. Бирблевская, д.51, 1";
+            string docNumber = "E-09945591";
             DateTime orderDate = DateTime.Now;
+
+            string productName = "Кабель мини USB-USB 2.0 FinePower";
             byte posCont = 5; //количество позиций в чеке
             double posSumm = 11000.33; //Сумма 
 
-            shopName = shopName.PadRight(33,' ');
+            byte stdLength = 60;
 
-            Console.WriteLine("----------------------------------------------");
-            Console.WriteLine($"| Продавец: {shopName}|");
-            Console.WriteLine($"| Дата фискального чека: {orderDate} |");
-            Console.WriteLine("|".PadRight(45,' ')+"|");
-            Console.WriteLine("|".PadRight(45, ' ') + "|");
-            Console.WriteLine($"| Количество товара: {posCont:d1}".PadRight(45,' ') + "|");
-            Console.WriteLine($"| Стоимость, руб: {posSumm:f2}".PadRight(45, ' ') + "|");
-            Console.WriteLine($"| в т.ч. НДС, руб: {(posSumm/6):f2}".PadRight(45, ' ') + "|");
-            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("-".PadRight(60,'-'));
+            Console.WriteLine($"| Продавец: {shopName}".PadRight(stdLength-1, ' ')+"|");
+            Console.WriteLine($"| Продавец: {shopAddress}".PadRight(stdLength - 1, ' ') + "|");
+            Console.WriteLine($"| Дата фискального чека: {orderDate}".PadRight(stdLength-1, ' ')+"|");
+            Console.WriteLine("|".PadRight(stdLength - 1, ' ') + "|");
+            Console.WriteLine("|".PadRight(stdLength - 1, ' ') + "|");
+            Console.WriteLine($"| Наименовение товара: {productName}".PadRight(stdLength - 1, ' ') + "|");
+            Console.WriteLine($"| Количество товара: {posCont:d1}".PadRight(stdLength - 1, ' ') + "|");
+            Console.WriteLine($"| Стоимость, руб: {posSumm:f2}".PadRight(stdLength - 1, ' ') + "|");
+            Console.WriteLine($"| в т.ч. НДС, руб: {(posSumm/6):f2}".PadRight(stdLength - 1, ' ') + "|");
+            Console.WriteLine("".PadRight(stdLength, '-'));
 
             Console.ReadKey();
         }
