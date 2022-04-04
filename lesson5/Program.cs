6755dd826e275cffeaaa2b9bd7837923ba5080c6
kevlampiev <kevlampiev@gmail.com>
2022-04-04 18:18:34 +0700

    задачи 1-3 выполнены

---
+++ lesson5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson5
+{
+    internal class Program
+    {
+              
+        static void Main(string[] args)
+        {
+            (new StringsWriter(1, "task5.1.txt")).Execute();
+            (new TimeWriter(2, "task5.2.txt")).Execute();
+            DigitsWriter dr = new DigitsWriter(3, "task5.3.dat");
+            dr.Execute();
+            dr.ShowDigitFile();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ lesson5/TerminalApp.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson5
+{
+    /// <summary>
+    /// Класс для установления единых цветов, стиля оформления и при выводе
+    /// </summary>
+    abstract class TerminalApp
+    {
+        /// <summary>
+        /// Цвет вывода сообщения об ошибке в консоли
+        /// </summary>
+        public ConsoleColor ErrorColor { get; set; }
+        /// <summary>
+        /// Цвет вывода заголовка в (тина названия задачи, шапки таблицы)
+        /// </summary>
+        public ConsoleColor HeaderColor { get; set; }
+        /// <summary>
+        /// Цвет вывода заголовка задачи
+        /// </summary>
+        public ConsoleColor TaskHeaderColor { get; set; }
+        /// <summary>
+        /// Последовательность символов, которую надо ввести с клавиатуры, чтобы закончить ввод списка строк
+        /// </summary>
+        public string TerminationString { get; set;  }
+
+        private string _taskName;
+
+        public TerminalApp(int taskNo) 
+        { 
+            ErrorColor = ConsoleColor.DarkRed;
+            HeaderColor = ConsoleColor.Cyan;
+            TaskHeaderColor = ConsoleColor.White;
+            TerminationString = ":q";
+            _taskName = $"Задание {taskNo}";
+            DisplayTaskHeader();
+        }
+
+        /// <summary>
+        /// Выводит заголовок правильным цветом
+        /// </summary>
+        /// <param name="header">Текст заголовка</param>
+        public void DisplayHeader(string header) 
+        { 
+            Console.ForegroundColor = HeaderColor;
+            Console.WriteLine(header);
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Выводит ошибку заданным цветом (ErrorColor)
+        /// </summary>
+        /// <param name="err">Текст ошибки</param>
+        public void DisplayError(string err) 
+        {
+            Console.ForegroundColor = ErrorColor;
+            Console.WriteLine(err);
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Отображает заголовок задачи
+        /// </summary>
+        protected void DisplayTaskHeader()
+        {
+            Console.ForegroundColor = TaskHeaderColor;
+            Console.WriteLine($"=================== {_taskName} ===================");
+            Console.ResetColor();
+        }
+
+        public abstract void Execute();
+        
+
+    }
+}
