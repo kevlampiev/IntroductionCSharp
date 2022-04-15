359d3d8af775376e04aaed3d85033267cfbbfff2
kevlampiev <kevlampiev@gmail.com>
2022-04-15 21:18:34 +0700

    Lesson 8

---
+++ KEvlampiev.TerminalUtils/TerminalApp.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KEvlampiev.TerminalUtils
+{
+    public static class TerminalApp
+    {
+        /// <summary>
+        /// Цвет вывода сообщения об ошибке в консоли
+        /// </summary>
+        public static ConsoleColor ErrorColor { get; set; }
+        /// <summary>
+        /// Цвет вывода заголовка в (тина названия задачи, шапки таблицы)
+        /// </summary>
+        public static ConsoleColor HeaderColor = ConsoleColor.Cyan;
+        /// <summary>
+        /// Цвет вывода заголовка задачи
+        /// </summary>
+        public static ConsoleColor TaskHeaderColor = ConsoleColor.White;
+        /// <summary>
+        /// Последовательность символов, которую надо ввести с клавиатуры, чтобы закончить ввод списка строк
+        /// </summary>
+        public static string TerminationString { get; set; }
+
+        private static string _taskName;
+
+
+        public static string TaskName { get { return _taskName; } set { _taskName = value; } }
+
+        /// <summary>
+        /// Выводит заголовок правильным цветом
+        /// </summary>
+        /// <param name="header">Текст заголовка</param>
+        public static void DisplayHeader(string header)
+        {
+            Console.ForegroundColor = HeaderColor;
+            Console.WriteLine(header);
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Выводит ошибку заданным цветом (ErrorColor)
+        /// </summary>
+        /// <param name="err">Текст ошибки</param>
+        public static void DisplayError(string err)
+        {
+            Console.ForegroundColor = ErrorColor;
+            Console.WriteLine(err);
+            Console.ResetColor();
+        }
+
+        /// <summary>
+        /// Отображает заголовок задачи
+        /// </summary>
+        public static void DisplayTaskHeader(string taskName, string userName)
+        {
+            Console.ForegroundColor= TaskHeaderColor;
+            Console.WriteLine($"=================== {taskName} ===================");
+            Console.WriteLine($"Выполнил: {userName}");
+            Console.ResetColor();
+        }
+
+
+        public static int AskInteger(string prompt)
+        {
+            Console.Write(prompt + ": ");
+            if (int.TryParse(Console.ReadLine(), out int result))
+            {
+                return result;
+            }
+            else
+            {
+                DisplayError("Ошибка : введенный набор символов не является числом. Будет возвращено значение -1 .... ");
+                return -1;
+            }
+        }
+
+        public static string AskString(string prompt) 
+        {
+            Console.Write(prompt + ": ");
+            return Console.ReadLine(); 
+        }
+
+        public static void WaitBeforeExit() 
+        {
+            Console.WriteLine("\n Для завершения нажмите любую клавишу");
+            Console.ReadKey(true);
+        }
+    }
+
+}
---
+++ lesson8a/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("lesson8a")]
+[assembly: AssemblyDescription("Домашнее задание к уроку 8")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lesson8a")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("cf11ae03-de3f-4705-aaec-fe9071ecf779")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
